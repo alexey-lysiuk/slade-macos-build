2bfab49e4d20b8b571f105d447a7c3dd6c50bd86
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-10-03 16:45:59 +0300

    + script for making a disk image

---
+++ make_dmg.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o errexit
+
+cd "`dirname \"$0\"`"
+./build.sh "$1"
+
+DEPLOY_DIR=deploy
+
+mkdir "$DEPLOY_DIR"
+cp -r build/slade.app "$DEPLOY_DIR/SLADE.app"
+cp SLADE/gpl-2.0.txt "$DEPLOY_DIR"
+ln -s /Applications "$DEPLOY_DIR/Applications"
+
+cd SLADE
+DEPLOY_IMAGE_NAME=SLADE-`git describe --tags`
+cd ..
+
+hdiutil create -srcfolder "$DEPLOY_DIR" -volname "$DEPLOY_IMAGE_NAME" \
+        -format UDBZ -fs HFS+ -fsargs "-c c=64,a=16,e=16" \
+        "$DEPLOY_IMAGE_NAME.dmg"
